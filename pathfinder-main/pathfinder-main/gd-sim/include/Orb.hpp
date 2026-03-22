82d12254038f5f3eec3d8faade288fade71ef549
Lythron-AI <lean.sp16@gmail.com>
2026-03-22 15:36:58 -0300

    Build artifact: pathfinder-main/pathfinder-main/gd-sim/include/Orb.hpp

---
+++ pathfinder-main/pathfinder-main/gd-sim/include/Orb.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <EffectObject.hpp>
+
+enum class OrbType {
+	Yellow,
+	Blue,
+	Pink,
+	Red,
+	Green,
+	Black,
+};
+
+struct Orb : public EffectObject {
+	OrbType type;
+
+	Orb(Vec2D size, std::unordered_map<int, std::string>&& fields);
+	bool touching(Player const&) const override;
+	void collide(Player&) const override;
+};
